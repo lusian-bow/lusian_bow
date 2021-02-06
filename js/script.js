7301a2ffc2894732d87cba51c8a756df4e19b256
lusian_bow <61625495+lusian-bow@users.noreply.github.com>
2021-02-06 16:02:11 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(window).on('load',function(){
   // ここから文字を<span></span>で囲む記述
-  $('.typ').children().andBack().contents().each(function() {
+  $('.typ').children().addBack().contents().each(function() {
   if (this.nodeType == 3) {
   $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
   }
@@ -8,7 +8,7 @@ $(window).on('load',function(){
   // ここから一文字ずつフェードインさせる記述
   $('.typ').css({'opacity':1});
   for (var i = 0; i <= $('.typ').children().size(); i++) {
-  $('.typ').children('span:eq('+i+')').delay(60*i).animate({'opacity':1},50);
+  $('.typ').children('span:eq('+i+')').delay(70*i).animate({'opacity':1},70);
   };
   });
 
